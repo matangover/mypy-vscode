bfafe14907d8af7c8cf8226b672c469a03555625
Matan Gover <matangover@gmail.com>
2019-05-06 18:23:23 -0400

    Show mypy processing indicator.

--- src/extension.ts
+++ src/extension.ts
@@ -4,48 +4,40 @@
  * ------------------------------------------------------------------------------------------ */
 'use strict';
 
-import * as net from 'net';
 
-import { workspace, Disposable, ExtensionContext } from 'vscode';
-import { LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions, ErrorAction, ErrorHandler, CloseAction, TransportKind } from 'vscode-languageclient';
+import { workspace, ExtensionContext, StatusBarItem, window } from 'vscode';
+import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient';
 
-function startLangServer(command: string, args: string[], documentSelector: string[]): Disposable {
-	const serverOptions: ServerOptions = {
-        command,
-        args,
-	};
-	const clientOptions: LanguageClientOptions = {
-		documentSelector: documentSelector,
-        synchronize: {
-            configurationSection: "pyls"
-        }
-	}
-	return new LanguageClient(command, serverOptions, clientOptions).start();
-}
-
-function startLangServerTCP(addr: number, documentSelector: string[]): Disposable {
-	const serverOptions: ServerOptions = function() {
-		return new Promise((resolve, reject) => {
-			var client = new net.Socket();
-			client.connect(addr, "127.0.0.1", function() {
-				resolve({
-					reader: client,
-					writer: client
-				});
-			});
-		});
-	}
-
-	const clientOptions: LanguageClientOptions = {
-		documentSelector: documentSelector,
-	}
-	return new LanguageClient(`tcp lang server (port ${addr})`, serverOptions, clientOptions).start();
-}
+let statusBarItem: StatusBarItem;
 
 export function activate(context: ExtensionContext) {
     const executable = workspace.getConfiguration("pyls").get<string>("executable");
-    context.subscriptions.push(startLangServer(executable, ["-vv"], ["python"]));
-    // For TCP server needs to be started seperately
-    // context.subscriptions.push(startLangServerTCP(2087, ["python"]));
+	const serverOptions: ServerOptions = {
+        command: executable || 'pyls',
+        args: ['-v'],
+	};
+	const clientOptions: LanguageClientOptions = {
+		documentSelector: ['python'],
+        synchronize: {
+            configurationSection: 'pyls'
+        }
+	}
+	const client = new LanguageClient('pyls', serverOptions, clientOptions);
+
+	context.subscriptions.push(client.start());
+
+	statusBarItem = window.createStatusBarItem();
+	context.subscriptions.push(statusBarItem);
+	
+	client.onReady().then(() => {
+		client.onNotification('pyls/reportProgress', (message: string | null) => {
+			if (message) {
+				statusBarItem.text = message;
+				statusBarItem.show();
+			} else {
+				statusBarItem.hide();
+			}
+		});
+	})
 }
 
